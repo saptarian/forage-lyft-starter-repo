c64d95ec895e19316494871ab7faa9e36fdb3eba
saptarian <saptaqur@gmail.com>
2023-07-30 21:25:58 +0700

    Refactoring the code

---
+++ car_factory.py
@@ -0,0 +1,23 @@
+from car import Car
+
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        # return Car object
+
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        # return Car object
+
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_is_on):
+        # return Car object
+
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        # return Car object
+
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        # return Car object
